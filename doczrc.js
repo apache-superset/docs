8d8a440d68e0b12781ab009d4880284c1e545524
kxu <kevin.s.xu@gmail.com>
2020-08-24 12:36:53 -0700

    Update doc menus

--- doczrc.js
+++ doczrc.js
@@ -5,8 +5,14 @@ export default {
   ignore: ['**/blog/**', 'readme.md'],
   files: ['src/**/*.{md,markdown,mdx}'],
   menu: [
-    'Getting Started',
-    'Architecture', 
-    ['Front End', 'Back End'],
+    'Installation',
+    'Database Connectors',
+    'Architecture Overview',
+    'Build Your Own Viz Plugins',
+    'Learning Resources',
+    'Best Practices',
+    'FAQs',
+    'Contribution',
+    'Roadmap',
   ],
 }
